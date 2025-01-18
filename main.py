fb12218607872d3426c17a580ef7144754b372b6
Dushyant Kaushal <122849580+RebelAKL@users.noreply.github.com>
2025-01-18 07:32:52 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+from pdf2docx import Converter
+
+input_folder = 'docs/' 
+output_folder = 'output/'
+os.makedirs(output_folder, exist_ok=True)
+
+for file_name in os.listdir(input_folder):
+    if file_name.endswith('.pdf'):
+        pdf_file = os.path.join(input_folder, file_name)
+        docx_file = os.path.join(output_folder, file_name.replace('.pdf', '.docx'))
+
+        print(f'Converting: {file_name} to {docx_file}')
+
+        cv = Converter(pdf_file)
+        cv.convert(docx_file)
+        cv.close()
+
+print("Success!")
