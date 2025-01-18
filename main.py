d9496a494d8ee9a579950095f86cfdfd9d32ca2e
RebelAKL <rebel7soul@duck.com>
2025-01-18 07:39:07 +0530

    success

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import os
 from pdf2docx import Converter
 
-input_folder = 'docs/' 
-output_folder = 'output/'
-os.makedirs(output_folder, exist_ok=True)
+def convert_pdfs_to_docx(input_folder, output_folder):
+    os.makedirs(output_folder, exist_ok=True)
 
-for file_name in os.listdir(input_folder):
-    if file_name.endswith('.pdf'):
-        pdf_file = os.path.join(input_folder, file_name)
-        docx_file = os.path.join(output_folder, file_name.replace('.pdf', '.docx'))
+    for file_name in os.listdir(input_folder):
+        if file_name.endswith('.pdf'):
+            pdf_file = os.path.join(input_folder, file_name)
+            docx_file = os.path.join(output_folder, file_name.replace('.pdf', '.docx'))
 
-        print(f'Converting: {file_name} to {docx_file}')
+            print(f'Converting: {file_name} to {docx_file}')
 
-        cv = Converter(pdf_file)
-        cv.convert(docx_file)
-        cv.close()
+            cv = Converter(pdf_file)
+            cv.convert(docx_file)
+            cv.close()
 
-print("Success!")
+    print("Success!")
+
+
+if __name__ == "__main__":
+    input_folder = 'docs/'  
+    output_folder = 'output/' 
+    convert_pdfs_to_docx(input_folder, output_folder) 
